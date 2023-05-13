f7d68ff59ec547a032472891e4026b3c44583a0e
Suson Waiba <susonwaiba@gmail.com>
2023-05-13 15:54:39 +0545

    added tests dir, empty for now

---
+++ tests/SimpleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @copyright Suson Waiba. All rights reserved.
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleTest extends TestCase
+{
+    public function testSimple(): void
+    {
+        $this->assertTrue(true);
+    }
+}
